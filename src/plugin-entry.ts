334d193a6d8d9ce2ef790acd8bb589c65c55b125
EkaTech <contact@ekatech.net>
2026-08-04 02:18:06 +0300

    Use media-aware plugin entrypoint

---
+++ src/plugin-entry.ts
@@ -0,0 +1,31 @@
+import type { WorkspaceLeaf } from 'obsidian';
+import { GALLERY_VIEW_TYPE } from './constants';
+import { AlbumGalleryView } from './gallery-view';
+import AlbumGalleryPlugin from './main';
+import { installGalleryMediaSupport } from './media-support';
+
+interface RegisterViewHost {
+	registerView(type: string, viewCreator: (leaf: WorkspaceLeaf) => unknown): void;
+}
+
+export default class AlbumGalleryMediaPlugin extends AlbumGalleryPlugin {
+	async onload(): Promise<void> {
+		const host = this as unknown as RegisterViewHost;
+		const originalRegisterView = host.registerView.bind(this);
+		host.registerView = (type, viewCreator) => {
+			originalRegisterView(type, (leaf) => {
+				const view = viewCreator(leaf);
+				if (type === GALLERY_VIEW_TYPE && view instanceof AlbumGalleryView) {
+					installGalleryMediaSupport(view);
+				}
+				return view;
+			});
+		};
+
+		try {
+			await super.onload();
+		} finally {
+			host.registerView = originalRegisterView;
+		}
+	}
+}
