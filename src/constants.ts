c54d95e59fdcdf00a10eb83395442feeeaba3af5
EkaTech <contact@ekatech.net>
2026-08-03 19:29:13 +0300

    Define managed gallery asset constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,20 @@
 export const GALLERY_VIEW_TYPE = 'album-gallery-view';
 export const GALLERY_EXTENSION = 'gallery';
 export const DEFAULT_GALLERY_BASENAME = 'Untitled gallery';
+export const ASSET_ROOT_FOLDER = 'Album Gallery Assets';
+export const FILE_PICKER_ACCEPT = 'image/*,.heic,.heif,.tif,.tiff';
 
 export const SUPPORTED_IMAGE_EXTENSIONS = new Set([
 	'avif',
 	'bmp',
 	'gif',
+	'heic',
+	'heif',
 	'jpeg',
 	'jpg',
 	'png',
 	'svg',
+	'tif',
+	'tiff',
 	'webp',
 ]);
