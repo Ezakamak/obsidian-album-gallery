000f603960f01bf818da1dc46b563e5e3f17ce61
EkaTech <contact@ekatech.net>
2026-08-03 18:20:01 +0300

    Add gallery constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const GALLERY_VIEW_TYPE = 'album-gallery-view';
+export const GALLERY_EXTENSION = 'gallery';
+export const DEFAULT_GALLERY_BASENAME = 'Untitled gallery';
+
+export const SUPPORTED_IMAGE_EXTENSIONS = new Set([
+	'avif',
+	'bmp',
+	'gif',
+	'jpeg',
+	'jpg',
+	'png',
+	'svg',
+	'webp',
+]);
